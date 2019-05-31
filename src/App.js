6296c0dbf30681f178486f7cce6678039013430c
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2019-05-30 22:06:18 -0300

    criado docker files

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, it's amazing!
         </a>
       </header>
     </div>
